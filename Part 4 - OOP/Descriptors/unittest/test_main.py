936904a0a71c5fc6ce00884378572a7fc89dacb9
Iuliia Shal <julia.d.shal@gmail.com>
2023-08-23 13:00:04 +1200

    Tests for TypeError
    Create tests for age and name TypeErrors

--- Part 4 - OOP/Descriptors/unittest/test_main.py
+++ Part 4 - OOP/Descriptors/unittest/test_main.py
@@ -24,10 +24,28 @@ def test_create_invalid_age(age_value):
         p.age = age_value
 
 @pytest.mark.parametrize('name_value',['','sdhjbsfjsfjdsbfjsdfjdsjfsdjfhgsjkdfnshjgsfhgjkdfdsfgfhgtrhbvcbcvfdssdfsefasnfkgndfkgnrtg'])
-def test_create_invalid_age(name_value):
+def test_create_invalid_name(name_value):
     '''
     testing 2 different values for name attribute
     '''
     p = main.Person()
     with pytest.raises(ValueError):
+        p.name = name_value
+
+@pytest.mark.parametrize('age_value',[10.5, 'abc', (1,2), 1 + 0j])
+def test_create_invalid_age_type(age_value):
+    '''
+    testing 2 different values for age attribute
+    '''
+    p = main.Person()
+    with pytest.raises(TypeError):
+        p.age = age_value
+
+@pytest.mark.parametrize('name_value',[10.5, 15, (1,2), 1 + 0j])
+def test_create_invalid_name_type(name_value):
+    '''
+    testing 2 different values for age attribute
+    '''
+    p = main.Person()
+    with pytest.raises(TypeError):
         p.name = name_value
